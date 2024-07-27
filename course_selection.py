9def158cc2c5f52713237bce34cc2c7c3efad187
voldermortik <utkarsh.raj@interviewkickstart.com>
2024-07-28 03:12:45 +0530

    Create course_selection.py

---
+++ course_selection.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Function to read HTML file
+def load_html(file_path):
+    with open(file_path, 'r') as file:
+        return file.read()
+
+# Load HTML content
+html_string = load_html('https://github.com/voldermortik/Course-Selection-Tool/blob/main/course_selection.html')  
+# Replace 'index.html' with the path to your HTML file
+
+# Embed HTML content in Streamlit app
+components.html(html_string, height=800, scrolling=True)
